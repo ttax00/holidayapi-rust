a4601549a325e8858c608d09fc6f58d6c94597f6
TechTheAwesome <techtheawesome01@gmail.com>
2022-09-22 16:27:36 +0900

    REFACTOR invalid api key errors

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ mod responses;
 use requests::{
     CountriesRequest, Endpoint, HolidaysRequest, LanguagesRequest, WorkdayRequest, WorkdaysRequest,
 };
-use std::{collections::HashMap, error::Error};
+use std::{collections::HashMap, error::Error, fmt};
 
 use regex::Regex;
-use reqwest::{Response, Url};
+use reqwest::{Response, StatusCode, Url};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone)]
@@ -19,9 +19,21 @@ pub struct HolidayAPI {
 
 #[derive(Debug)]
 pub enum HolidayAPIError {
-    InvalidKey,
+    InvalidKeyFormat(String),
+    InvalidOrExpiredKey(String),
     InvalidVersion(String),
 }
+
+impl fmt::Display for HolidayAPIError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidKeyFormat(key) => write!(f, "Invalid key: {}", key),
+            Self::InvalidVersion(version) => write!(f, "Invalid version: {}", version),
+            Self::InvalidOrExpiredKey(key) => write!(f, "Invalid or expired key: {}", key),
+        }
+    }
+}
+impl Error for HolidayAPIError {}
 
 #[derive(strum_macros::Display, Debug, Clone, Serialize, Deserialize)]
 pub enum Format {
@@ -59,14 +71,14 @@ impl HolidayAPI {
 
     pub fn new(key: &str) -> Result<HolidayAPI, HolidayAPIError> {
         if !Self::is_valid_key(key) {
-            return Err(HolidayAPIError::InvalidKey);
+            return Err(HolidayAPIError::InvalidKeyFormat(key.into()));
         }
         Ok(Self::construct_api(key, 1))
     }
 
     pub fn with_version(key: &str, version: i32) -> Result<HolidayAPI, HolidayAPIError> {
         if !Self::is_valid_key(key) {
-            return Err(HolidayAPIError::InvalidKey);
+            return Err(HolidayAPIError::InvalidKeyFormat(key.into()));
         }
         if let Some(error) = Self::is_valid_version(version) {
             return Err(HolidayAPIError::InvalidVersion(error));
@@ -87,7 +99,13 @@ impl HolidayAPI {
         let response = client.get(url).send().await?;
         match response.error_for_status() {
             Ok(res) => Ok(res),
-            Err(err) => Err(Box::new(err)),
+            Err(err) => match err.status() {
+                Some(StatusCode::UNAUTHORIZED) => Err(Box::new(
+                    HolidayAPIError::InvalidOrExpiredKey(self.key.clone()),
+                )),
+                Some(_) => Err(Box::new(err)),
+                None => unreachable!(),
+            },
         }
     }
     pub fn countries(&self) -> CountriesRequest {
@@ -122,20 +140,17 @@ mod tests {
     #[ignore]
     async fn test_countries_api() {
         let api = HolidayAPI::new(EXPIRED_KEY).unwrap();
-        let response = api
-            .countries()
-            .country("us")
-            .public(true)
-            .get()
-            .await
-            .unwrap();
+        let response = api.countries().country("us").public(true).get().await;
         println!("{:?}", response);
     }
 
     #[tokio::test]
     async fn test_holidays_api() {
         let api = HolidayAPI::new(EXPIRED_KEY).unwrap();
-        let response = api.holidays("jp", 2021).pretty(true).get().await.unwrap();
-        eprintln!("{:?}", response);
+        let response = api.holidays("jp", 2021).pretty(true).get().await;
+        match response {
+            Ok(res) => eprintln!("{:?}", res),
+            Err(res) => eprintln!("{}", res),
+        }
     }
 }
